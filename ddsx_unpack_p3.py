3e823f51e0cb4cb33094f571a97a775cacf7b17e
mike dolan <michael08dolan@gmail.com>
2016-07-10 17:43:20 -0400

    Added Python 3 support
    added versions of the scripts with _p3 at the end to indicate that they
    are for python 3.

---
+++ ddsx_unpack_p3.py
@@ -0,0 +1,107 @@
+import struct, sys, pylzma, ctypes, zlib
+import os.path
+
+ddsx_types = ['DXT1', 'DXT5']
+dds_header = [
+    0x44, 0x44, 0x53, 0x20, 0x7C, 0x00, 0x00, 0x00,
+    0x07, 0x10, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00,
+    0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    ]
+
+
+def unpack(data):
+    """
+    Unpack data from ddsx and returns it. If data have wrong header, prints error
+    and exit(1). Return unpacked dds data, ready for saving.
+
+    :param data: ddsx data
+    """
+    header_format = struct.unpack_from('4s', data, 0x4)[0]
+    if header_format not in ddsx_types:
+        print ('wrong ddsx type:', header_format)
+        exit(1)
+
+    dds_height = struct.unpack_from('H', data, 0xc)[0]
+    dds_width = struct.unpack_from('H', data, 0xe)[0]
+    dds_mipmapcount = struct.unpack_from('B', data, 0x10)[0]
+    dds_unpacked_body_size = struct.unpack_from('I', data, 0x18)[0]
+    dds_body_size = struct.unpack_from('I', data, 0x1c)[0]
+    ddsx_unknown_flag_0 = struct.unpack_from('B', data, 0xa)
+    if ddsx_unknown_flag_0 in [0, 1]:
+        pass  # all unpack ok 11 c0 01 40, 11 40 01 40, 11 40 00 40
+
+    dds_data = ctypes.create_string_buffer(0x80)
+    struct.pack_into('128B', dds_data, 0, *dds_header)
+    struct.pack_into('I', dds_data, 0xc, dds_width)
+    struct.pack_into('I', dds_data, 0x10, dds_height)
+    struct.pack_into('I', dds_data, 0x14, dds_unpacked_body_size)
+    struct.pack_into('B', dds_data, 0x1c, dds_mipmapcount)
+    struct.pack_into('4s', dds_data, 0x54, header_format)
+
+    if dds_body_size == 0:  # not packed
+        return dds_data.raw + data[0x20:]
+    elif struct.unpack_from('I', data, 0x20)[0] == 0x1000005d:  # packed with lzma
+        return dds_data.raw + pylzma.decompress(data[0x20:], maxlength=dds_unpacked_body_size)
+    else:  # packed with zlib
+        return dds_data.raw + zlib.decompress(data[0x20:])
+
+
+def unpack_file(filename):
+    # TODO: eliminate copy&paste with blk_unpack
+    with open(filename, 'rb') as f:
+        data = f.read()
+    if len(data) == 0:
+        print ("empty file")
+        return
+    out_file = unpack(data)
+    with open(filename[:-1], 'wb') as f:
+        f.write(out_file)
+
+
+def _unpack_dir(arg, dirname, names):
+    """
+    Func to os.path.walk for unpack files with 'arg' mod
+    """
+    for name in names:
+        subname = os.path.join(dirname, name)
+        if os.path.isfile(subname) and os.path.splitext(subname)[1] == '.ddsx':
+            print (subname)
+            unpack_file(subname)
+
+
+def unpack_dir(dirname):
+    """
+    Unpack all files in `dirname`
+    """
+    os.path.walk(dirname, _unpack_dir, None)
+
+
+def main():
+    if len(sys.argv) != 2:
+        print ('usage: ddsx_unpack.py PATH\n' \
+              'where PATH - file or folder')
+        sys.exit(1)
+
+    filename = sys.argv[1]
+
+    if os.path.isfile(filename):
+        unpack_file(filename)
+    else:
+        unpack_dir(filename)
+
+
+if __name__ == '__main__':
+    main()
---
+++ dxp_unpack_p3.py
@@ -0,0 +1,99 @@
+import struct, sys, os, errno
+
+
+dxp2_magic = 'DxP2'
+file_names_block_offset = 0x48
+second_block_offset_from = 0x10  # + 0x10
+dds_block_offset_from = 0x20  # + 0x10
+block_3_offset_from = 0x30  # + 0x10
+block_4_offset_from = 0xc  # + 0x10
+
+
+def mkdir_p(path):
+    n_path = ''.join(os.path.split(path)[:-1])
+    try:
+        if n_path != '':
+            os.makedirs(n_path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(n_path):
+            pass
+        else:
+            raise
+
+
+def main():
+    if len(sys.argv) != 2:
+        print ('usage: dxp_unpack.py file')
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    dist_dir = filename + '_u/'
+
+    data = []
+    with open(filename, 'rb') as f:
+        data = f.read()
+
+    if len(data) == 0:
+        print ("empty file")
+        exit(1)
+
+    if struct.unpack_from('4s', data, 0)[0] != dxp2_magic:
+        print ("wrong dxp type")
+        exit(1)
+
+    total_files = struct.unpack_from('H', data, 0x8)[0]
+    print ("total files:", total_files)
+    cur_p = file_names_block_offset
+
+    file_names = []
+    for i in xrange(total_files):
+        old_cur_p = cur_p
+        while ord(data[cur_p]) != 0x0:
+            cur_p += 1
+        file_names.append(data[old_cur_p: cur_p])
+        cur_p += 1
+
+    for i in file_names:
+        print (i)
+
+    cur_p = struct.unpack_from('I', data, second_block_offset_from)[0] + 0x10
+    offsets_block_1 = []
+    for i in xrange(total_files):
+        offsets_block_1.append(struct.unpack_from('I', data, cur_p)[0])
+        cur_p += 0x8
+    # print offsets_block_1
+
+    '''
+    from end:
+        0x4: unpacked size
+        0x4: packed size
+    '''
+    cur_p = struct.unpack_from('I', data, dds_block_offset_from)[0] + 0x10
+    dds_block = []
+    for i in xrange(total_files):
+        dds_block.append(data[cur_p: cur_p + 0x20])
+        cur_p += 0x20
+    # print dds_block
+
+    '''
+        0xc - offset
+        0x10 - size
+    '''
+    cur_p = struct.unpack_from('I', data, block_3_offset_from)[0] + 0x10
+    block_3 = []
+    for i in xrange(total_files):
+        offset = struct.unpack_from('I', data, cur_p + 0xc)[0]
+        size = struct.unpack_from('I', data, cur_p + 0x10)[0]
+        block_3.append((offset, size))
+        cur_p += 0x18
+    # print block_3
+
+    mkdir_p(dist_dir)
+    for i, (off, size) in enumerate(block_3):
+        with open(dist_dir + file_names[i].split('*')[0] + '.ddsx', 'wb') as f:
+            f.write(dds_block[i])
+            f.write(data[off: off + size])
+
+
+if __name__ == '__main__':
+    main()
