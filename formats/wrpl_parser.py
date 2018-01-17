ae9bf848db45b2c786e1015be8915ed56105d09b
klensy <klensy@users.noreply.github.com>
2018-01-17 22:49:29 +0300

    style fixes

--- formats/wrpl_parser.py
+++ formats/wrpl_parser.py
@@ -1,12 +1,13 @@
 import struct
-from construct import *
+
+from construct import Enum, Int16ul, Const, Struct, Int32ub, Int32ul, Bytes, this, Int24ul, Seek
 
 from formats.common import zlib_stream
 
 
 # there should be better way to build this, but i don't know it, for now
 def simple_blk_build(obj):
-    return "".join([obj.magic, struct.pack('>I', obj.unknown_0), struct.pack('<I', obj.blk_body_size), obj.blk_body])
+    return b"".join([obj.magic, struct.pack('>I', obj.unknown_0), struct.pack('<I', obj.blk_body_size), obj.blk_body])
 
 # i think not byte, but word\dword
 wrpl_version = "wrpl_version" / Enum(Int16ul,
@@ -14,8 +15,8 @@ wrpl_version = "wrpl_version" / Enum(Int16ul,
                                      version_1_47=0x88c8,
                                      version_1_49=0x88ca,
                                      version_1_51=0x88e7,
-                                     version_1_63=0x8964,
-                                     default=Error)
+                                     version_1_63=0x8964
+                                     )
 
 simple_blk = "blk" / Struct(
     "magic" / Const(b"\x00BBF"),
@@ -37,4 +38,3 @@ wrpl_file = "wrpl" / Struct(
     "wrplu" / zlib_stream,
     "rez" / simple_blk,
 )
-
