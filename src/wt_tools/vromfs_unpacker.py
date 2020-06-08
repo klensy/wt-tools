10fac1d541faee73fcea9e1474446696b12f6f94
klensy <klensy@users.noreply.github.com>
2020-06-08 17:28:52 +0300

    vromfs_unpacker: added --metadata option
    Can be used to output info about files in vromfs {filename: md5_hash}, if you want to get info about files and unpack only changed files, for example.

--- src/wt_tools/vromfs_unpacker.py
+++ src/wt_tools/vromfs_unpacker.py
@@ -1,5 +1,8 @@
 import errno
+import json
 import os
+from hashlib import md5
+from typing import Optional
 
 import click
 
@@ -31,29 +34,56 @@ def unpack(filename: os.PathLike, dist_dir: os.PathLike):
                 f.write(parsed.body.file_data_table.file_data_list[i].data)
 
 
+def files_list_info(filename: os.PathLike, dist_file: Optional[os.PathLike]) -> Optional[str]:
+    with open(filename, 'rb') as f:
+        data = f.read()
+    parsed = vromfs_file.parse(data)
+    out_list = []
+
+    for i in range(parsed.body.files_count):
+        m = md5(parsed.body.file_data_table.file_data_list[i].data).hexdigest()
+        out_list.append({"filename": os.path.normcase(parsed.body.filename_table.filenames[i]), "hash": m})
+    out_json = json.dumps({'version': 1, 'filelist': out_list})
+    if not dist_file:
+        return out_json
+    else:
+        with open(dist_file, 'w') as f:
+            f.write(out_json)
+
+
 @click.command()
 @click.argument('filename', type=click.Path(exists=True, dir_okay=False))
-@click.option('-O', '--output', 'output_dir', type=click.Path(file_okay=False), default=None)
-def main(filename: os.PathLike,  output_dir):
+@click.option('-O', '--output', 'output_path', type=click.Path(), default=None)
+@click.option('--metadata', 'metadata', is_flag=True, default=False)
+def main(filename: os.PathLike, output_path: os.PathLike, metadata: bool):
     """
     vromfs_unpacker: unpacks vromfs file into folder
 
     FILENAME: vromfs file to unpack
 
-    output: directory where to unpack vromfs file, by default is FILENAME with appended _u, like some.vromfs.bin_u
+    -O, --output: path where to unpack vromfs file, by default is FILENAME with appended _u, like some.vromfs.bin_u
+
+    --metadata: if present, prints metadata of vromfs file: json with filename: md5_hash. If --output option used,
+     prints to file instead.
 
     example: `vromfs_unpacker some.vromfs.bin` will unpack content to some.vromfs.bin_u folder. If you want to unpack to
     custom folder, use `vromfs_unpacker some.vromfs.bin --output my_folder`, that will unpack some.vromfs.bin folder to
-    my_folder.
+    my_folder. If you want to get only file metadata, use `vromfs_unpacker some.vromfs.bin --metadata`
     """
-    # unpack all into some.vromfs.bin_u folder
-    if not output_dir:
-        head, tail = os.path.split(filename)
-        output_dir = os.path.join(head, tail + '_u')
-    # else unpack into output_folder/some.vromfs.bin folder
+    if metadata:
+        if output_path:
+            files_list_info(filename, dist_file=output_path)
+        else:
+            print(files_list_info(filename, dist_file=None))
     else:
-        output_dir = os.path.join(output_dir, os.path.basename(filename))
-    unpack(filename, output_dir)
+        # unpack into output_folder/some.vromfs.bin folder
+        if output_path:
+            output_path = os.path.join(output_path, os.path.basename(filename))
+        # unpack all into some.vromfs.bin_u folder
+        else:
+            head, tail = os.path.split(filename)
+            output_path = os.path.join(head, tail + '_u')
+        unpack(filename, output_path)
 
 
 if __name__ == '__main__':
