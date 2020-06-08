ef62e5505c473abcf77155f86f5e4061c5628127
klensy <klensy@users.noreply.github.com>
2020-06-08 18:21:10 +0300

    vromfs_unpacker: added --input_filelist option
    Can be used to unpack only files, that you want to unpack, filelist should be passed as file in json list format, for example `["buildtstamp", "gamedata/units/tankmodels/fr_b1_ter.blk"]`

--- src/wt_tools/vromfs_unpacker.py
+++ src/wt_tools/vromfs_unpacker.py
@@ -21,17 +21,40 @@ def mkdir_p(path):
             raise
 
 
-def unpack(filename: os.PathLike, dist_dir: os.PathLike):
+def unpack(filename: os.PathLike, dist_dir: os.PathLike, file_list_path: Optional[os.PathLike] = None):
+    """
+    Unpacks files from vromfs
+
+    :param filename: path to vromfs file
+    :param dist_dir: path to output dir
+    :param file_list_path: path to file list, if you want to unpack only few files, in json list.
+    """
     with open(filename, 'rb') as f:
         data = f.read()
     parsed = vromfs_file.parse(data)
 
+    # want to unpack only listed files
+    if file_list_path:
+        with open(file_list_path, 'r') as f:
+            file_list = json.load(f)
+
+        # normalise paths in inputted list
+        file_list = [os.path.normcase(p) for p in file_list]
+
     with click.progressbar(range(parsed.body.files_count), label="Unpacking files") as bar:
         for i in bar:
-            unpacked_filename = os.path.join(dist_dir, parsed.body.filename_table.filenames[i])
-            mkdir_p(unpacked_filename)
-            with open(unpacked_filename, 'wb') as f:
-                f.write(parsed.body.file_data_table.file_data_list[i].data)
+            vromfs_internal_file_path = parsed.body.filename_table.filenames[i]
+            if file_list_path:
+                if os.path.normcase(vromfs_internal_file_path) in file_list:
+                    unpacked_filename = os.path.join(dist_dir, vromfs_internal_file_path)
+                    mkdir_p(unpacked_filename)
+                    with open(unpacked_filename, 'wb') as f:
+                        f.write(parsed.body.file_data_table.file_data_list[i].data)
+            else:
+                unpacked_filename = os.path.join(dist_dir, vromfs_internal_file_path)
+                mkdir_p(unpacked_filename)
+                with open(unpacked_filename, 'wb') as f:
+                    f.write(parsed.body.file_data_table.file_data_list[i].data)
 
 
 def files_list_info(filename: os.PathLike, dist_file: Optional[os.PathLike]) -> Optional[str]:
@@ -55,7 +78,8 @@ def files_list_info(filename: os.PathLike, dist_file: Optional[os.PathLike]) -> 
 @click.argument('filename', type=click.Path(exists=True, dir_okay=False))
 @click.option('-O', '--output', 'output_path', type=click.Path(), default=None)
 @click.option('--metadata', 'metadata', is_flag=True, default=False)
-def main(filename: os.PathLike, output_path: os.PathLike, metadata: bool):
+@click.option('--input_filelist', 'input_filelist', type=click.Path(), default=None)
+def main(filename: os.PathLike, output_path: Optional[os.PathLike], metadata: bool, input_filelist: Optional[os.PathLike]):
     """
     vromfs_unpacker: unpacks vromfs file into folder
 
@@ -64,11 +88,16 @@ def main(filename: os.PathLike, output_path: os.PathLike, metadata: bool):
     -O, --output: path where to unpack vromfs file, by default is FILENAME with appended _u, like some.vromfs.bin_u
 
     --metadata: if present, prints metadata of vromfs file: json with filename: md5_hash. If --output option used,
-     prints to file instead.
+    prints to file instead.
+
+    --input_filelist: pass the file with list of files you want to unpack and only this files will be unpacked.
+    Files should be a json list format, like: `["buildtstamp", "gamedata/units/tankmodels/fr_b1_ter.blk"]`
 
     example: `vromfs_unpacker some.vromfs.bin` will unpack content to some.vromfs.bin_u folder. If you want to unpack to
     custom folder, use `vromfs_unpacker some.vromfs.bin --output my_folder`, that will unpack some.vromfs.bin folder to
-    my_folder. If you want to get only file metadata, use `vromfs_unpacker some.vromfs.bin --metadata`
+    my_folder. If you want to get only file metadata, use `vromfs_unpacker some.vromfs.bin --metadata`. If you want to
+    unpack only few selected manually files, place json list of files in file, and use
+    `vromfs_unpacker some.vromfs.bin --input_filelist my_filelist.txt`
     """
     if metadata:
         if output_path:
@@ -83,7 +112,7 @@ def main(filename: os.PathLike, output_path: os.PathLike, metadata: bool):
         else:
             head, tail = os.path.split(filename)
             output_path = os.path.join(head, tail + '_u')
-        unpack(filename, output_path)
+        unpack(filename, output_path, input_filelist)
 
 
 if __name__ == '__main__':
